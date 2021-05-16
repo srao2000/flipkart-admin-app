0f5f34f3081342f33c1c671fd8ebc193d61b9285
Sanjeev <sanjeevyadav3004@gmail.com>
2021-05-16 08:41:50 +0530

    adding new files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './containers/Home';
+import Signin from './containers/Signin';
+import Signup from './containers/Signup';
+import PrivateRoute from './components/HOC/PrivateRoute';
+import { useDispatch, useSelector } from 'react-redux';
+import { isUserLoggedIn, getInitialData } from './actions';
+import Products from './containers/Products';
+import Orders from './containers/Orders';
+import Category from './containers/Category';
+import NewPage from './containers/NewPage';
+
+
 
 function App() {
+
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth)
+
+
+  //componentDidMount or componentDidUpdate
+  useEffect(() => {
+    if (!auth.authenticate) {
+      dispatch(isUserLoggedIn());
+    }
+    if(auth.authenticate){
+      dispatch(getInitialData());
+    }
+    
+
+  }, [auth.authenticate]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <PrivateRoute path="/" exact component={Home} />
+        <PrivateRoute path="/page" component={NewPage} />
+        <PrivateRoute path="/category" component={Category} />
+        <PrivateRoute path="/products" component={Products} />
+        <PrivateRoute path="/orders" component={Orders} />
+        
+
+
+        <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
     </div>
   );
 }
